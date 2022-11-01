a5fbec0d34583252cfc3b3a8ccbaf4b961de015d
wzmiiiiii <wzmiiiiii@zw.com>
2022-11-02 03:58:40 +0800

    221101

---
+++ src/zwngkey.cn/dsaa/bt/n_queen_problem/solution_one/leetcode51.go
@@ -0,0 +1,74 @@
+/*
+ * @Author: wzmiiiiii
+ * @Date: 2022-11-01 23:26:26
+ * @LastEditors: wzmiiiiii
+ * @LastEditTime: 2022-11-02 02:33:21
+ * @Description:
+	n 皇后问题 研究的是如何将 n 个皇后放置在 n × n 的棋盘上，并且使皇后彼此之间不能相互攻击。即：任意两个皇后都不能处于同一行、同一列或同一斜线上，
+
+	给你一个整数 n ，返回 n 皇后问题 不同的解决方案的数量。
+
+*/
+package main
+
+import "fmt"
+
+/*
+	根据题目描述:
+		将 N 个皇后放置在N×N的棋盘上，一定是每一行有且仅有一个皇后，每一列有且仅有一个皇后，且任何两个皇后都不能在同一条斜线上。
+
+	回溯的具体做法是：
+		依次在每一行放置一个皇后，每次新放置的皇后都不能和已经放置的皇后之间有攻击，
+			即新放置的皇后不能和任何一个已经放置的皇后在同一列以及同一条斜线上。
+			当 N 个皇后都放置完毕，则找到一个可能的解，将可能的解的数量加 1。
+
+	快速判断每个位置是否可以放置皇后?
+		为了判断一个位置所在的列和两条斜线上是否已经有皇后，使用三个数组分别记录每一列以及两个方向的每条斜线上是否有皇后。
+
+		列的表示法很直观，一共有 N 列，每一列的下标范围从 0 到 N−1，使用列的下标即可明确表示每一列。
+
+		如何表示两个方向的斜线呢？
+			对于每个方向的斜线，需要找到斜线上的每个位置的行下标与列下标之间的关系。
+				*从左上到右下方向，同一条斜线上的每个位置满足行下标与列下标之差相等
+				*从右上到左下方向，同一条斜线上的每个位置满足行下标与列下标之和相等
+*/
+
+func totalNQueens(n int) (ans int) {
+	columns := make([]bool, n)        // 记录某个列上是否有皇后
+	diagonals1 := make([]bool, 2*n-1) // 记录某条左上到右下的斜线上是否有皇后
+	diagonals2 := make([]bool, 2*n-1) // 记录某条右下到左上的斜线上是否有皇后
+
+	var backtrack func(int)
+
+	backtrack = func(row int) {
+		if row == n {
+			ans++
+			return
+		}
+		// 使用回溯模拟依次在棋盘的每一行放置一个皇后
+		for col := 0; col < n; col++ {
+			d1 := row - col + n - 1 //row-col范围为[-(n-1),n+1],row-col+n-1范围为[0,2n-2]->2n-1个数.
+			d2 := row + col         //row-col范围[0,2n-2]->2n-1个数.
+			// 判断当前位置所在的列和两条斜线上是否已经有皇后
+			if columns[col] || diagonals1[d1] || diagonals2[d2] {
+				continue
+			}
+			//如果没有,将当前位置所在列以及所在斜线标记为已有皇后
+			columns[col] = true
+			diagonals1[d1] = true
+			diagonals2[d2] = true
+			backtrack(row + 1) //递归,对下一行进行放置
+			columns[col] = false
+			diagonals1[d1] = false
+			diagonals2[d2] = false
+		}
+	}
+	backtrack(0)
+	return
+}
+
+func main() {
+	var n int
+	fmt.Scanf("%d", &n)
+	fmt.Printf("totalNQueens(n): %v\n", totalNQueens(n))
+}
---
+++ src/zwngkey.cn/dsaa/bt/n_queen_problem/solution_two/leetcode51.go
@@ -0,0 +1,78 @@
+/*
+ * @Author: wzmiiiiii
+ * @Date: 2022-11-01 16:41:14
+ * @LastEditors: wzmiiiiii
+ * @LastEditTime: 2022-11-02 01:22:46
+ * @Description:
+	在n × n格的国际象棋上摆放n个皇后，使其不能互相攻击，即：任意两个皇后都不能处于同一行、同一列或同一斜线上，问有多少种摆法。
+
+	如何判断在同一斜线上? 两点连成的线斜率为1.
+		即如果点A(x1,y1)与点B(x2,y2)满足 |x1-x2|÷|y1-y2| = 1.那么我们就说A,B两点在同一斜线上.
+
+*/
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// 记录棋盘行列
+	var size int
+
+	fmt.Scanf("%d", &size)
+
+	// **这种解法的核心**
+	// 通过数组下标(代表所在行),和这个下标存储的值(代表列) 来代表 一个皇后正确摆放的位置
+	// 通过整个数组来记录一种解法.
+	var arr = make([]int, size)
+
+	// 记录所有解法
+	var result [][]int
+
+	var bt func(int)
+
+	bt = func(row int) {
+		if row == size { //探测完了最后一行,确定了一种解法,记录这种解法.
+			temp := make([]int, size)
+			copy(temp, arr)
+			result = append(result, temp)
+			return //开始回溯到前面的行.在前面的行中寻找另一种解法.
+		}
+		// 递归深度就是row控制棋盘的行，每一层里for循环的col控制棋盘的列
+
+		// 函数一开始进入for循环,row不变,即行不变,通过col变化即列不断变化.来回溯试探皇后可以放置的正确位置.
+		for col := 0; col < size; col++ {
+			arr[row] = col       // 探测第row行的不同列是否存在正确位置.
+			if judge(row, arr) { // 第row行第col列可以放置皇后
+				// n不断变化,行不断变化.
+				bt(row + 1) //递归试探row+1行的皇后放置位置
+			}
+			// 若当前for自然结束,说明第k行不同列没有正确的摆放位置,就开始回溯.
+			// 这里回溯的结果就是回退到第k-1行,第k-1行就会i++,即探测第k-1行的下一列.
+		}
+
+	}
+	// 第一个皇后放在第一行第一列的位置.
+	bt(0)
+
+	// 输出每一种解法
+	for _, res := range result {
+		for i, v := range res {
+			fmt.Printf("{%d,%d} ", i+1, v+1)
+		}
+		fmt.Println()
+	}
+
+}
+
+// 判断第n个皇后,是否与前面n-1个皇后冲突.
+func judge(n int, arr []int) bool {
+	for i := 0; i < n; i++ {
+		if arr[i] == arr[n] || math.Abs(float64(i-n)) == math.Abs(float64(arr[i]-arr[n])) {
+			return false
+		}
+	}
+	return true
+}
